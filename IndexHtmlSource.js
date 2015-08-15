4989393ab2b07c616f0c00c5b7ae329611f8ae1b
Till Krullmann <till.krullmann@gmx.net>
2015-08-15 22:03:34 +0200

    Extended HTML code resolution to better work with hot module replacement and extract-text-plugin

--- IndexHtmlSource.js
+++ IndexHtmlSource.js
@@ -28,24 +28,110 @@ IndexHtmlSource.prototype.source = function() {
     return $.html();
 };
 
+if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function(searchString, position) {
+        var subjectString = this.toString();
+        if (position === undefined || position > subjectString.length) {
+            position = subjectString.length;
+        }
+        position -= searchString.length;
+        var lastIndex = subjectString.indexOf(searchString, position);
+        return lastIndex !== -1 && lastIndex === position;
+    };
+}
+
 /**
  * Extracts the HTML code from the module source
  */
 IndexHtmlSource.prototype._getHtmlFromModule = function() {
-    var code = this.sourceModule.source(null, {}).source()
-        .replace(/^module\.exports\s*=\s*/, '');
 
-    //noinspection JSUnusedLocalSymbols
-    var __webpack_require__ = function (moduleId) {
-        var module = find(this.sourceChunk.modules, function (module) {
-            return module.id === moduleId
-        });
-        return find(module.chunks[0].files, function (file) {
-            return file.match(/\.css$/)
-        });
-    }.bind(this);
+    var compilation = this.compilation;
+    var sourceChunk = this.sourceChunk;
 
-    return eval(code);
+
+    function moduleWasExtracted(module) {
+        return module.loaders && module.loaders.some(function(loader) {
+                return loader.match(/extract-text-webpack-plugin/);
+            })
+    }
+
+
+    function getExtractTextLoaderOptions(module) {
+        for (var i = 0; i < module.loaders.length; i++) {
+            var loader = module.loaders[i];
+            var match = loader.match(/extract-text-webpack-plugin[\\/]loader\.js\?({.*})/);
+            if (match) {
+                return JSON.parse(match[1]);
+            }
+        }
+    }
+
+
+    function getExtractedFilename(module) {
+
+        var options = getExtractTextLoaderOptions(module);
+        var loaderId = options && options.id;
+
+        var extractTextPlugin = find(compilation.compiler.options.plugins,
+            function (p) {
+                return (p.constructor.name === 'ExtractTextPlugin') &&
+                    ((typeof(p.id) === "undefined" && typeof(loaderId) === "undefined") || (p.id === loaderId));
+            });
+
+        var filenamePattern = extractTextPlugin.filename
+            .replace(/\[(?:\w+:)?(contenthash)(?::[a-z]+\d*)?(?::(\d+))?]|([^\[\]]+)/ig,
+            function(match, contentHash, maxLength, literalPart) {
+                if (contentHash) {
+                    if (maxLength) {
+                        return '[a-f0-9]{1,' + maxLength + '}';
+                    } else {
+                        return '[a-f0-9]+';
+                    }
+                } else {
+                    return regexpQuote(literalPart);
+                }
+            });
+        filenamePattern = new RegExp(filenamePattern);
+
+        return find(sourceChunk.files, function(filename) {
+            return filename.match(filenamePattern);
+        });
+    }
+
+
+    function __webpack_require__(moduleId) {
+
+        var sourceModule;
+        if (typeof moduleId === "number") {
+            sourceModule = find(compilation.modules, function (m) {
+                return m.id === moduleId;
+            });
+        } else {
+            sourceModule = moduleId;
+        }
+
+        if (!sourceModule) {
+            return undefined;
+        }
+
+        if (sourceModule.context.endsWith(path.normalize('webpack-dev-server/client')) ||
+            sourceModule.context.endsWith(path.normalize('webpack/hot'))) {
+            return undefined;
+
+        } else if (moduleWasExtracted(sourceModule)) {
+            return getExtractedFilename(sourceModule);
+
+        } else {
+            var module = {};
+
+            var source = sourceModule.source(null, {});
+            eval(source.source());
+
+            return module.exports;
+        }
+    }
+
+    return __webpack_require__(this.sourceModule);
 };
 
 
@@ -93,6 +179,11 @@ function find(array, predicate) {
         }
     }
     return undefined;
+}
+
+
+function regexpQuote(s) {
+    return s.toString().replace(/[-\\^$*+?.()|[\]{}]/g, "\\$&");
 }
 
 
