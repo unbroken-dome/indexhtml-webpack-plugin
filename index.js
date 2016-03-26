8da67c9d851be4d378ea0c8f140a619b78803bc0
Neil Kronlage <neil.kronlage@gmail.com>
2016-03-26 16:06:11 -0700

    Update plugin to handle multiple .html entry points

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 var IndexHtmlSource = require('./IndexHtmlSource');
 
-
-function IndexHtmlPlugin(source, target) {
-
+function IndexHtmlPlugin() {
     this.apply = function apply(compiler) {
 
         compiler.plugin('this-compilation', function (compilation) {
 
+
             compilation.plugin('additional-chunk-assets', function additionalChunkAssets() {
 
-                var sourceChunk = this.namedChunks[source];
-                var sourceModule = sourceChunk.origins[0].module;
+                Object.keys(this.namedChunks).forEach(function (source) {
+                    if (/\.html$/.test(source)) {
+                        var sourceChunk = this.namedChunks[source];
+                        var sourceModule = sourceChunk.origins[0].module;
 
-                var filePath = this.getPath(target, {chunk: sourceChunk});
-                this.additionalChunkAssets.push(filePath);
-                this.assets[filePath] = new IndexHtmlSource(sourceModule, sourceChunk, this);
-                sourceChunk.files.push(filePath);
+                        var filePath = this.getPath(source, { chunk: sourceChunk });
+                        this.additionalChunkAssets.push(filePath);
+                        this.assets[filePath] = new IndexHtmlSource(sourceModule, sourceChunk, this);
+                        sourceChunk.files.push(filePath);
+                    }
+                }, this);
             });
         });
 
         compiler.plugin('emit', function emit(compilation, callback) {
 
             Object.keys(compilation.assets).forEach(function (file) {
+                Object.keys(compilation.namedChunks).forEach(function (source) {
+                    if (/\.html$/.test(source)) {
+                        var targetFile = file;
+                        var queryStringIdx = targetFile.indexOf('?');
+                        if (queryStringIdx >= 0) {
+                            targetFile = targetFile.substr(0, queryStringIdx);
+                        }
 
-                var targetFile = file;
-                var queryStringIdx = targetFile.indexOf('?');
-                if (queryStringIdx >= 0) {
-                    targetFile = targetFile.substr(0, queryStringIdx);
-                }
-
-                if (targetFile === source + '.js' || targetFile === source + '.js.map') {
-                    delete compilation.assets[file];
-                }
+                        if (targetFile === source + '.js' || targetFile === source + '.js.map') {
+                            delete compilation.assets[file];
+                        }
+                    }
+                });
             });
 
             callback();
